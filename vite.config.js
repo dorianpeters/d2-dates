b10358a0f4310f9bd8c04687ab0f9375d5037599
Dorian Peters <dpeters08@gmail.com>
2025-05-28 09:43:41 -0700

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 export default {
   base: '/trial-deadlines/',
+  minify: true,
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].min.js',
+      },
+    },
   }
 }
