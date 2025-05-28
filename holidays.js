65f694a67fd5b45a8754ef65128b8f52f2ae4ab0
Dorian Peters <dpeters08@gmail.com>
2025-05-28 09:09:31 -0700

    Added holidays through 2035.

--- holidays.js
+++ holidays.js
@@ -23,7 +23,7 @@ export const holidaySet = new Set([
   "2026-03-31", // Cesar Chavez Day
   "2026-05-25", // Memorial Day
   "2026-06-19", // Juneteenth Day
-  "2026-07-03", // Independence Day (Observed)
+  "2026-07-03", // Independence Day (Observed - Sat, Jul 4)
   "2026-09-07", // Labor Day
   "2026-09-25", // Native American Day
   "2026-11-11", // Veterans Day
@@ -38,15 +38,15 @@ export const holidaySet = new Set([
   "2027-02-15", // Presidents' Day
   "2027-03-31", // Cesar Chavez Day
   "2027-05-31", // Memorial Day
-  "2027-06-18", // Juneteenth Day (Observed)
-  "2027-07-05", // Independence Day (Observed)
+  "2027-06-18", // Juneteenth Day (Observed - Sat, Jun 19)
+  "2027-07-05", // Independence Day (Observed - Sun, Jul 4)
   "2027-09-06", // Labor Day
   "2027-09-24", // Native American Day
   "2027-11-11", // Veterans Day
   "2027-11-25", // Thanksgiving Day
   "2027-11-26", // Day after Thanksgiving
-  "2027-12-24", // Christmas Day (Observed)
-  "2027-12-31", // New Year's Day (Observed)
+  "2027-12-24", // Christmas Day (Observed - Sat, Dec 25)
+  "2027-12-31", // New Year's Day (Observed - Sat, Jan 1, 2028)
 
   // 2028 Holidays
   "2028-01-17", // Martin Luther King Jr. Day
@@ -58,8 +58,120 @@ export const holidaySet = new Set([
   "2028-07-04", // Independence Day
   "2028-09-04", // Labor Day
   "2028-09-22", // Native American Day
-  "2028-11-10", // Veterans Day (Observed)
+  "2028-11-10", // Veterans Day (Observed - Sat, Nov 11)
   "2028-11-23", // Thanksgiving Day
   "2028-11-24", // Day after Thanksgiving
-  "2028-12-25"  // Christmas Day
+  "2028-12-25", // Christmas Day
+
+  // 2029 Holidays
+  "2029-01-01", // New Year's Day
+  "2029-01-15", // Martin Luther King Jr. Day
+  "2029-02-12", // Lincoln's Birthday
+  "2029-02-19", // Presidents' Day
+  "2029-03-30", // Cesar Chavez Day (Observed - Sun, Mar 31)
+  "2029-05-28", // Memorial Day
+  "2029-06-19", // Juneteenth Day
+  "2029-07-04", // Independence Day
+  "2029-09-03", // Labor Day
+  "2029-09-28", // Native American Day
+  "2029-11-12", // Veterans Day (Observed - Sun, Nov 11)
+  "2029-11-22", // Thanksgiving Day
+  "2029-11-23", // Day after Thanksgiving
+  "2029-12-25", // Christmas Day
+
+  // 2030 Holidays
+  "2030-01-01", // New Year's Day
+  "2030-01-21", // Martin Luther King Jr. Day
+  "2030-02-12", // Lincoln's Birthday
+  "2030-02-18", // Presidents' Day
+  "2030-04-01", // Cesar Chavez Day (Observed - Sun, Mar 31)
+  "2030-05-27", // Memorial Day
+  "2030-06-19", // Juneteenth Day
+  "2030-07-04", // Independence Day
+  "2030-09-02", // Labor Day
+  "2030-09-27", // Native American Day
+  "2030-11-11", // Veterans Day
+  "2030-11-28", // Thanksgiving Day
+  "2030-11-29", // Day after Thanksgiving
+  "2030-12-25", // Christmas Day
+
+  // 2031 Holidays
+  "2031-01-01", // New Year's Day
+  "2031-01-20", // Martin Luther King Jr. Day
+  "2031-02-12", // Lincoln's Birthday
+  "2031-02-17", // Presidents' Day
+  "2031-03-31", // Cesar Chavez Day
+  "2031-05-26", // Memorial Day
+  "2031-06-19", // Juneteenth Day
+  "2031-07-04", // Independence Day
+  "2031-09-01", // Labor Day
+  "2031-09-26", // Native American Day
+  "2031-11-11", // Veterans Day
+  "2031-11-27", // Thanksgiving Day
+  "2031-11-28", // Day after Thanksgiving
+  "2031-12-25", // Christmas Day
+
+  // 2032 Holidays
+  "2032-01-01", // New Year's Day
+  "2032-01-19", // Martin Luther King Jr. Day
+  "2032-02-12", // Lincoln's Birthday
+  "2032-02-16", // Presidents' Day
+  "2032-03-31", // Cesar Chavez Day
+  "2032-05-31", // Memorial Day
+  "2032-06-18", // Juneteenth Day (Observed - Sat, Jun 19)
+  "2032-07-05", // Independence Day (Observed - Sun, Jul 4)
+  "2032-09-06", // Labor Day
+  "2032-09-24", // Native American Day
+  "2032-11-11", // Veterans Day
+  "2032-11-25", // Thanksgiving Day
+  "2032-11-26", // Day after Thanksgiving
+  "2032-12-24", // Christmas Day (Observed - Sat, Dec 25)
+  "2032-12-31", // New Year's Day (Observed - Sat, Jan 1, 2033)
+
+  // 2033 Holidays
+  "2033-01-17", // Martin Luther King Jr. Day
+  "2033-02-11", // Lincoln's Birthday (Observed - Sat, Feb 12)
+  "2033-02-21", // Presidents' Day
+  "2033-03-31", // Cesar Chavez Day
+  "2033-05-30", // Memorial Day
+  "2033-06-20", // Juneteenth Day (Observed - Sun, Jun 19)
+  "2033-07-04", // Independence Day
+  "2033-09-05", // Labor Day
+  "2033-09-23", // Native American Day
+  "2033-11-11", // Veterans Day
+  "2033-11-24", // Thanksgiving Day
+  "2033-11-25", // Day after Thanksgiving
+  "2033-12-26", // Christmas Day (Observed - Sun, Dec 25)
+
+  // 2034 Holidays
+  "2034-01-02", // New Year's Day (Observed - Sun, Jan 1)
+  "2034-01-16", // Martin Luther King Jr. Day
+  "2034-02-13", // Lincoln's Birthday (Observed - Sun, Feb 12)
+  "2034-02-20", // Presidents' Day
+  "2034-03-31", // Cesar Chavez Day
+  "2034-05-29", // Memorial Day
+  "2034-06-19", // Juneteenth Day
+  "2034-07-04", // Independence Day
+  "2034-09-04", // Labor Day
+  "2034-09-22", // Native American Day
+  "2034-11-10", // Veterans Day (Observed - Sat, Nov 11)
+  "2034-11-23", // Thanksgiving Day
+  "2034-11-24", // Day after Thanksgiving
+  "2034-12-25", // Christmas Day
+
+  // 2035 Holidays
+  "2035-01-01", // New Year's Day
+  "2035-01-15", // Martin Luther King Jr. Day
+  "2035-02-12", // Lincoln's Birthday
+  "2035-02-19", // Presidents' Day
+  "2035-03-30", // Cesar Chavez Day (Observed - Sun, Mar 31)
+  "2035-05-28", // Memorial Day
+  "2035-06-19", // Juneteenth Day
+  "2035-07-04", // Independence Day
+  "2035-09-03", // Labor Day
+  "2035-09-28", // Native American Day
+  "2035-11-12", // Veterans Day (Observed - Sun, Nov 11)
+  "2035-11-22", // Thanksgiving Day
+  "2035-11-23", // Day after Thanksgiving
+  "2035-12-25", // Christmas Day
 ]);
